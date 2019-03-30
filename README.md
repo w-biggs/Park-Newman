f271928092f88fa99f534442354a7b302616d6ee
w-biggs <wilson@wilsonbiggs.com>
2019-03-29 20:28:14 -0400

    Add dynamicness - decay scores over time

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 > [Park-Newman network-based ranking](https://arxiv.org/abs/physics/0505169), implemented in JavaScript.
 
 This is somewhat simplified - especially the calculation of the parameter *a* - as a lot of the linear algebra stuff in that paper is way over my head. But the end result is very similar, and the general process is the same.
+
+This is also modified to decay scores over time, similarly to the dynamic variant of the Park-Newman ranking outlined in [this paper](https://www.nature.com/articles/srep00904).
--- pn.js
+++ pn.js
@@ -1,44 +1,84 @@
-const data = require('./wl.json');
+const teams = require('./teams.json');
+const data = require('./games.json');
 
 // Avoid floating point errors
 const round = (input, places) => {
   return Number(Number(input).toFixed(places));
 }
 
-const winScore = (team, a, baseA) => {
+const winScore = (team, a, baseA, b) => {
   if(a < 0.0001){
     return 0;
   }
   let score = 0;
   const newA = round(a*baseA,4);
-  data[team].wins.forEach(opp => {
-    score += a;
-    score += winScore(opp, newA, baseA);
-  })
+  data.games.forEach(game => {
+    const hScore = Number(game.home.score);
+    const aScore = Number(game.away.score);
+    let gameScore = 0;
+    if(aScore > hScore){
+      if(game.away.name === team){
+        gameScore += a;
+        gameScore += winScore(game.home.name, newA, baseA, b);
+      } else {
+        return 0;
+      }
+    } else if (hScore > aScore){
+      if(game.home.name === team){
+        gameScore += a;
+        gameScore += winScore(game.away.name, newA, baseA, b);
+      } else {
+        return 0;
+      }
+    } else {
+      return 0;
+    }
+    score += gameScore * Math.exp(-b * (data.currentWeek - game.week)); 
+  });
   return round(score,4);
 }
 
-const lossScore = (team, a, baseA) => {
+const lossScore = (team, a, baseA, b) => {
   if(a < 0.0001){
     return 0;
   }
   let score = 0;
   const newA = round(a*baseA,4);
-  data[team].losses.forEach(opp => {
-    score -= a;
-    score -= lossScore(opp, newA, baseA);
-  })
+  data.games.forEach(game => {
+    const hScore = Number(game.home.score);
+    const aScore = Number(game.away.score);
+    let gameScore = 0;
+    if(aScore > hScore){
+      if(game.home.name === team){
+        gameScore -= a;
+        gameScore += lossScore(game.away.name, newA, baseA, b);
+      } else {
+        return 0;
+      }
+    } else if (hScore > aScore){
+      if(game.away.name === team){
+        gameScore -= a;
+        gameScore += lossScore(game.home.name, newA, baseA, b);
+      } else {
+        return 0;
+      }
+    } else {
+      return 0;
+    }
+    score += gameScore * Math.exp(-b * (data.currentWeek - game.week)); 
+  });
+  //console.log(score);
   return round(score,4);
 }
 
-Object.keys(data).forEach(team => {
+teams.fcs.forEach(team => {
   /**
   * In their paper, Park and Newman have a complex process using eigenvalues or something
   * to find the optimal value for *a*. But that value hovers around 0.2 for every year they
   * tested, so I'm gonna just use 0.2.
   */
-  const w = winScore(team, 1, 0.2);
-  const l = lossScore(team, 1, 0.2);
+  const w = winScore(team, 1, 0.2, 1/data.seasonLength);
+  const l = lossScore(team, 1, 0.2, 1/data.seasonLength);
   const wl = round(w + l, 4);
-  console.log(team, wl);
+
 })
