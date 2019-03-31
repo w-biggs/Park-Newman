c7e926a5d17d465f824d56dbed5f578b13fc12c2
w-biggs <wilson@wilsonbiggs.com>
2019-03-31 15:04:58 -0400

    Remove dynamicness

--- pn.js
+++ pn.js
@@ -9,7 +9,7 @@ const round = (input, places) => {
   return Number(Number(input).toFixed(places));
 }
 
-const winScore = (team, a, baseA, b) => {
+const winScore = (team, a, baseA) => {
   if(a < 0.0001){
     return 0;
   }
@@ -22,14 +22,14 @@ const winScore = (team, a, baseA, b) => {
     if(aScore > hScore){
       if(game.away.name === team){
         gameScore += a;
-        gameScore += winScore(game.home.name, newA, baseA, b);
+        gameScore += winScore(game.home.name, newA, baseA);
       } else {
         return 0;
       }
     } else if (hScore > aScore){
       if(game.home.name === team){
         gameScore += a;
-        gameScore += winScore(game.away.name, newA, baseA, b);
+        gameScore += winScore(game.away.name, newA, baseA);
       } else {
         return 0;
       }
@@ -38,12 +38,12 @@ const winScore = (team, a, baseA, b) => {
     }
     // weight by game length as well
     gameScore *= game.gameLength / 1680;
-    score += gameScore * Math.exp(-b * (data.currentWeek - game.week)); 
+    score += gameScore; 
   });
   return round(score,4);
 }
 
-const lossScore = (team, a, baseA, b) => {
+const lossScore = (team, a, baseA) => {
   if(a < 0.0001){
     return 0;
   }
@@ -56,14 +56,14 @@ const lossScore = (team, a, baseA, b) => {
     if(aScore > hScore){
       if(game.home.name === team){
         gameScore -= a;
-        gameScore += lossScore(game.away.name, newA, baseA, b);
+        gameScore += lossScore(game.away.name, newA, baseA);
       } else {
         return 0;
       }
     } else if (hScore > aScore){
       if(game.away.name === team){
         gameScore -= a;
-        gameScore += lossScore(game.home.name, newA, baseA, b);
+        gameScore += lossScore(game.home.name, newA, baseA);
       } else {
         return 0;
       }
@@ -72,7 +72,7 @@ const lossScore = (team, a, baseA, b) => {
     }
     // weight by game length as well
     gameScore *= game.gameLength / 1680;
-    score += gameScore * Math.exp(-b * (data.currentWeek - game.week)); 
+    score += gameScore; 
   });
   //console.log(score);
   return round(score,4);
@@ -84,8 +84,8 @@ teams.fcs.forEach(team => {
   * to find the optimal value for *a*. But that value hovers around 0.2 for every year they
   * tested, so I'm gonna just use 0.2.
   */
-  const w = winScore(team, 1, 0.2, 1/data.seasonLength);
-  const l = lossScore(team, 1, 0.2, 1/data.seasonLength);
+  const w = winScore(team, 1, 0.2);
+  const l = lossScore(team, 1, 0.2);
   const wl = round(w + l, 4);
   results.push([team,wl]);
 })
